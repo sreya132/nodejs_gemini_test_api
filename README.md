5e75ea5afe76201827f607a8c9e870da8c4546b8
Sreyashi Saha <sreyashi.saha123@gmail.com>
2025-10-26 17:50:56 +0530

    updated readme and main.js

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # nodejs_gemini_test_api
+### starting redme file
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 
+// adding express
 const app = express()
 app.use (express.json())
 app.use(bodyParser.json())
