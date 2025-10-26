d35bd74847ac09854524f36f2d2be608758a5e99
Sreyashi Saha <sreyashi.saha123@gmail.com>
2025-10-26 17:42:38 +0530

    initially added files

---
+++ main.js
@@ -0,0 +1,40 @@
+const { GoogleGenAI } = require( "@google/genai");
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use (express.json())
+app.use(bodyParser.json())
+
+app.get('/', (req,res)=>{
+   res.send("sending...")
+})
+
+const ai = new GoogleGenAI({});
+
+async function generate(prompt) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: prompt,
+  });
+  return response.text;
+}
+
+//generate();
+
+app.get('/api/content', async(req,res)=>{
+    try{
+        const data = req.body.question
+        const result = await generate(data)
+        res.send({
+            "result": result
+        })
+    }catch(err){
+        res.send("error", err);
+    }
+})
+
+app.listen(8080,()=>{
+    console.log("server is up on http://localhost:8080 ");
+})
